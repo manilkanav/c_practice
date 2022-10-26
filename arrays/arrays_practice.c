07919512df99c1ce1901f9e22ea5b83641a999da
Manilka Navod <manilkadikkumbura17@gmail.com>
2022-10-26 11:27:18 +0530

    added arrays_practice

---
+++ arrays/arrays_practice.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+    int arr[10], i;
+
+    for (i = 0; i < 10; i++) {
+        printf("> ");
+        scanf("%d", &arr[10]);
+    }
+
+    for (i = 0; i < 10; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
