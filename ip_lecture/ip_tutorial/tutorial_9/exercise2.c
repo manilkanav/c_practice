7e28ebad7df07fcb126ed1f1d6ae9615c453fed9
Manilka Navod <manilkadikkumbura17@gmail.com>
2022-12-07 00:32:43 +0530

    updated tests

--- ip_lecture/ip_tutorial/tutorial_9/exercise2.c
+++ ip_lecture/ip_tutorial/tutorial_9/exercise2.c
@@ -6,15 +6,15 @@ double hypotenuse(double s1, double s2);
 
 int main(void) {
     
-    assert(hypotenuse(3.0, 4.0) == 5);
-    assert(hypotenuse(5.0, 12.0) == 13);
-    assert(hypotenuse(8.0, 15.0) == 17);
+    assert(fabs(5.0 - hypotenuse(3, 4)) < 0.0001);
+    assert(fabs(13.0 - hypotenuse(5, 12)) < 0.0001);
+    assert(fabs(17.0 - hypotenuse(8, 15)) < 0.0001);
 
-    printf("Program ran succesfull!\n");
+    printf("Tests ran successfully!\n");
     return 0;
 }
 
-double hypotenuse(double s1, double s2) {
+double hypotenuse(double s1, double s2) { 
     // (s1 * s1 + s2 * s2)
     double ans = sqrt(s1 * s1 + s2 * s2);
 
